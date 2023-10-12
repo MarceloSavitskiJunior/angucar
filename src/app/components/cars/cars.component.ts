90293f6e6e4a0462bda4a5ac0f96877bbba69ca3
MarceloSavitskiJunior <marcelosavitskijunior@gmail.com>
2023-10-11 22:45:09 -0300

    ajustes para melhorar o desempenho

--- src/app/components/cars/cars.component.ts
+++ src/app/components/cars/cars.component.ts
@@ -14,7 +14,7 @@ export class CarsComponent {
     { marca: 'Chevrolet', image: '../../../assets/images/marcas/chevrolet.jpg' },
     { marca: 'BMW', image: '../../../assets/images/marcas/bmw-logo.jpg' },
     { marca: 'Volkswagen', image: '../../../assets/images/marcas/volkswagen-logo.jpg' },
-    { marca: 'Subaru', image: '../../../assets/images/marcas/subaru-logo.jpg' },
+    { marca: 'Subaru', image: '../../../assets/images/marcas/subaru-logo.png' },
     { marca: 'Toyota', image: '../../../assets/images/marcas/toyota-logo.jpg' },
     { marca: 'Fiat', image: '../../../assets/images/marcas/fiat-logo.jpg' }
   ];
